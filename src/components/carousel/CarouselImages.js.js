9181d9b7b0bd006bb01fb3430880a544e5e37b19
danonuevo09 <lhorkhann@gmail.com>
2022-07-02 16:34:32 +0800

    updated Carousel links

--- src/components/carousel/CarouselImages.js.js
+++ src/components/carousel/CarouselImages.js.js
@@ -18,16 +18,16 @@ class CarouselImages extends Component {
                     <div className={classes.image_container} >
                         <img className={classes.image} src={Hello} alt="myImage" />
                         <div className={classes.h1}>
-                            <h1 >I'm Sachin</h1>
-                            <a href="https://drive.google.com/file/d/1XwQYRl06QtuNPT8v4fHkbajk1ll9LTbi/view?usp=sharing" rel="opener noreferrer" target="_blank">VIEW CV   <FaArrowCircleDown /></a>
+                            <h1 >I'm Danel</h1>
+                            <a href="https://drive.google.com/file/d/1MtSjXlJmfBLYeXdfGTahwggfg74RPBjz/view?usp=sharing" rel="opener noreferrer" target="_blank">VIEW CV   <FaArrowCircleDown /></a>
                         </div>
                     </div>
                     <div className={classes.image_container}>
                         <img className={classes.image} src={Pencil} alt="myImage" />
                         <div className={classes.h2}>
-                            <h1> I love building</h1>
+                            <h1> I love building and exploring</h1>
                             <h1>THINGS!!</h1>
-                            <a href="https://github.com/mittalsam98" rel="opener noreferrer" target="_blank">VIEW PROJECTS   <GoProject /></a>
+                            <a href="#" rel="opener noreferrer" target="_blank">VIEW PROJECTS   <GoProject /></a>
                         </div>
                     </div>
                     {/* <div className={classes.image_container}>
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -12,10 +12,10 @@ class Sidebar extends Component {
                 <div style={{ color: 'black', fontWeight: 'bold' }} className="tagtop"> # programmer_life </div>
                 <div style={{ color: 'black', fontWeight: 'bold' }} className=""> # hello_world </div>
                 <div style={{ color: 'black', fontWeight: 'bold' }} className=""> # coding </div>
-                <h1><Link smooth to="/#start" className="h1_links">Sachin Mittal</Link></h1>
+                <h1><Link smooth to="/#start" className="h1_links">Daniel Añonuevo</Link></h1>
 
                 <img src={logo} />
-                <p style={{ color: 'black', fontWeight: 'bold' }} className="gmail"><a href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJftvBffjPmxWxvfHWRHWnhTbpQgPxBxvkcnPpFpFqbhrXMCHXSJTsCXCVLsQzgRcVLpnPL" rel="opener noreferrer" target="_blank" className="fa fa-envelope"></a> mittalsam98@gmail.com </p>
+                <p style={{ color: 'black', fontWeight: 'bold' }} className="gmail"><a href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJftvBffjPmxWxvfHWRHWnhTbpQgPxBxvkcnPpFpFqbhrXMCHXSJTsCXCVLsQzgRcVLpnPL" rel="opener noreferrer" target="_blank" className="fa fa-envelope"></a> danielanonuevo07@gmail.com </p>
 
                 <ul className="sidebar-nav">
                     <li className="sidebar-nav-items"><Link smooth to="/#about" className="links">About</Link></li>
